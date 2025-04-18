59c8c918981f85f4717d69cc4f58f00fb0537c0f
qingyun <qingyun@localhost.localdomain>
2025-04-18 13:54:13 +0800

    4.18 13:54

---
+++ os_exp2/pip/father.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+
+int main()
+{
+	pid_t pid1,pid2,pid3;	
+	key_t key;
+	int fd[2];
+	int smd;
+	char buff[BUFF_SIZE];
+	
+	return 0;
+}
