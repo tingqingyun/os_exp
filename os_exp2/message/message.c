12ff108889b3d7d434c9efa87282eb085931ee7d
qingyun <qingyun@localhost.localdomain>
2025-04-23 08:31:14 +0800

    4.23 8:31

---
+++ os_exp2/message/message.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<mqueue.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAX_SIZE 256
+mqd_t mq;
+sem_t sem_sender1,sem_sender2,sem_receive;
+
+void* sender1()
+{
+	char buffer[MAX_SIZE];
+	while(1)
+	{
+		printf("sender1: input your message\n");
+		scanf("%s",buffer);
+		if(strcmp(buffer,"exit")==0)
+		{
+			break;
+		}
+		sem_wait(&sem_sender1);
+		mq_send(mq,buffer,strlen(buffer),1);
+		sem_post(&sem_receive);
+	}
+	mq_send(mq,"end1",strlen("end1"),1);
+	sem_post(&sem_receive);
+}
+
+void* sender2()
+{
+	char buffer[MAX_SIZE];
+	while(1)
+	{
+		printf("sender2: input your message\n");
+		scanf("%s",buffer);
+		if(strcmp(buffer,"exit")==0)
+		{
+			break;
+		}
+		sem_wait(&sem_sender2);
+		mq_send(mq,buffer,strlen(buffer),1);
+		sem_post(&sem_receive);
+	}
+	mq_send(mq,"end2",strlen("end2"),1);
+	sem_post(&sem_receive);
+}
+
+void* receive()
+{
+	char buffer[MAX_SIZE];
+	while(1)
+	{
+		sem_wait(&sem_receive);
+		mq_receive(mq,buffer,MAX_SIZE,NULL);
+		printf("receive receiced message:%s\n",buffer);
+		if(strcmp(buffer,"end1")==0)
+		{
+			mq_send(mq,"over1",strlen("over1"),1);
+			sem_post(&sem_sender1);
+		}
+		else if(strcmp(buffer,"end2")==0)
+		{
+			mq_send(mq,"over2",strlen("over2"),1);
+			sem_post(&sem_sender2);
+			break;
+		}
+	}
+	mq_close(mq);
+	mq_unlink("/p");
+}
+
+int main()
+{
+	pthread_t sender1_thread,sender2_thread,receive_thread;
+	sem_init(&sem_sender1,0,0);
+	sem_init(&sem_sender2,0,0);
+	sem_init(&sem_receive,0,0);
+	struct mq_attr attr;
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize  = MAX_SIZE;
+	attr.mq_curmsgs = 0;
+	mq = mq_open("/p",O_CREAT | O_RDWR,0644,&attr);
+	if(mq == (mqd_t)-1)
+	{
+		perror("mq_open");
+		exit(0);
+	}
+	pthread_create(&sender1_thread,NULL,sender1,NULL);
+	pthread_create(&sender2_thread,NULL,sender2,NULL);
+	pthread_create(&receive_thread,NULL,receive,NULL);
+	pthread_join(sender1_thread,NULL);
+	pthread_join(sender2_thread,NULL);
+	pthread_join(receive_thread,NULL);
+	sem_destroy(&sem_sender1);
+	sem_destroy(&sem_sender2);
+	sem_destroy(&sem_receive);
+	return 0;
+}
